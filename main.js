f08da6fd406e45009deab0c58e4f0a51dfb61b41
Letch49 <Letchmagadan@gmail.com>
2019-02-27 21:28:41 +1000

    work version

---
+++ main.js
@@ -0,0 +1,69 @@
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(2));
+const fs = require('fs');
+const _ = require('./rsa');
+
+const main = (args) => {
+    console.log(args);
+    if (args['help']) {
+        const consoleMessages = {
+            '--generate': 'Сгенерировать открытый и закрытый ключ',
+            '--in': 'Путь к входному файлу',
+            '--e': 'зашифровать',
+            '--d': 'Расшифровать',
+            '--help': 'Справочная информация',
+        };
+        Object.keys(consoleMessages).forEach((el) => {
+            console.log(`${el}: `, `${consoleMessages[el]}`, '\n');
+        });
+        process.exit();
+    }
+    if (args['generate']) {
+        _.saveOpenKey();
+        _.savePrivateKey();
+    }
+
+    let fileIn= null;
+    if(args['in']) {
+        fileIn = args['in'];
+    }
+
+    if (args['e']) {
+        const openKey = JSON.parse(fs.readFileSync(__dirname+'/_open.json', {encoding: 'utf-8'}));
+        const e = _.hexToDec(openKey['e']);
+        const n = _.hexToDec(openKey['n']);
+        const privateKey = JSON.parse(fs.readFileSync(__dirname+'/_private.json', {encoding: 'utf-8'}));
+        const d = _.hexToDec(privateKey['d']);
+        fs.readFile(fileIn, {encoding : 'utf-8'} ,(err,message) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            message = _.encMessage(message)(e,n,d);
+            fs.writeFile(__dirname+'/enc.txt', message, (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    if (args['d']) {
+        const privateKey = JSON.parse(fs.readFileSync(__dirname+'/_private.json', {encoding: 'utf-8'}));
+        const d = _.hexToDec(privateKey['d']);
+        const n = _.hexToDec(privateKey['n']);
+        fs.readFile(fileIn, {encoding : 'utf-8'} ,(err,message) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            // console.log(message.split(','));
+            message = _.decMessage(message)(d,n);
+            fs.writeFile(__dirname+'/dec.txt', message, (err) => {
+                console.log(err);
+            });
+        });
+    }
+};
+
+main(args);
+
+// console.log(privatePair);
--- rsa.js
+++ rsa.js
@@ -2,7 +2,7 @@ const Crypto = require('crypto');
 const bigInt = require('big-integer');
 const fs = require('fs');
 
-const saveKey = (mode) => (num, n) => {
+const saveKey = (mode) => (num, n) => () => {
     [num, n] = [num.toString(16), n.toString(16)];
     if (mode === 'open') {
         const json = JSON.stringify({
@@ -24,66 +24,45 @@ const saveKey = (mode) => (num, n) => {
     return;
 };
 
+
+
+const p = bigInt(Crypto.createDiffieHellman(64).getPrime('hex'), 16);
+const q = bigInt(Crypto.createDiffieHellman(64).getPrime('hex'), 16);
+const n = p.multiply(q);
+const f = p.minus(1).multiply(q.minus(1));
+const d = bigInt(Crypto.createDiffieHellman(512).getPrime('hex'), 16);
+const e = bigInt(d).modInv(f);
+const delemiter = 100000;
+
+const messageToDigit = (message) => message.split('').map(e => e.charCodeAt());
+
 const enc = (e, n) => (code) => bigInt(code).modPow(e, n);
 
 const dec = (d, n) => (code) => bigInt(code).modPow(d, n);
 
-const p = bigInt(Crypto.createDiffieHellman(16).getPrime('hex'), 16);
-const q = bigInt(Crypto.createDiffieHellman(16).getPrime('hex'), 16);
-const n = p.multiply(q);
-const f = p.minus(1).multiply(q.minus(1));
-const e = bigInt(Crypto.createDiffieHellman(512).getPrime('hex'), 16);
-const d = bigInt(e).modInv(f);
+const messageToBlocks = (message, sep) => message.map(el => bigInt(el).multiply(sep.divide(delemiter)));
 
-const messageToDigit = (message) => message.split('').map(e => e.charCodeAt()).join('');
+const blocksToMessage = (message, sep) => message.map(el => bigInt(el).multiply(delemiter).divide(sep).plus(1));
 
-const messageToBlocks = (message, sep = n.bitLength().toString()) => {
-    const digitMsg = messageToDigit(message);
-    const blocks = [];
-    let state = '';
-    for (let i = 0; i < digitMsg.length; i++) {
-        if (digitMsg[i + 1] === undefined) {
-            state += digitMsg[i];
-            blocks.push(state);
-            break;
-        }
-        if (parseInt(state.concat(digitMsg[i + 1])) > sep) {
-            blocks.push(state);
-            state = '';
-        }
-        state += digitMsg[i];
-    }
+const encMessage = (message) => (key, n) => {
+    message = messageToDigit(message);
+    const blocks = messageToBlocks(message,n);
+    const enycrypt = blocks.map((el) => enc(key, n)(el)).join(',');
+    return enycrypt;
+};
+
+const decMessage = (message) => (key, n) => {
+    message = message.split(',').map(el => dec(key, n)(el));
+    const blocks = blocksToMessage(message,n).map(el => String.fromCharCode(el)).join('');
     return blocks;
 };
 
-const blocksToMessage = (digitMsg, sep = n.bitLength().toString()) => {
-    digitMsg = digitMsg.join('');
-    const blocks = [];
-    let state = '';
-    for (let i = 0; i < digitMsg.length; i++) {
-        if (digitMsg[i+1] === undefined) {
-            state += digitMsg[i];
-            blocks.push(state);
-            break;
-        }
-        if (parseInt(state) >= sep) {
-            blocks.push(state);
-            state = '';
-        }
-        state += digitMsg[i];
-    }
-    return blocks;
-};
-
+const hexToDec = (val) => bigInt(val, 16);
 
 module.exports = {
-    'e' : e,
-    'd' : d,
-    'n' : n,
-    'enc': enc(e,n),
-    'dec' : dec(d,n),
-    'savePrivateKey': saveKey('private'),
-    'saveOpenKey': saveKey('open'),
-    'messageToBlocks': messageToBlocks,
-    'blocksToMessage': blocksToMessage
+    'savePrivateKey': saveKey('private')(d, n),
+    'saveOpenKey': saveKey('open')(e, n),
+    'encMessage': encMessage,
+    'decMessage': decMessage,
+    'hexToDec': hexToDec
 };
